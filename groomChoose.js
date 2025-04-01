e4e1777971072e16c2a9d2cd945d2cba328a2c51
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-01 22:51:49 +0100

    condiotions more

---
+++ groomChoose.js
@@ -0,0 +1,10 @@
+const groomSalary = 25000;
+const groomHeight = 6.1;
+const isBcs = true;
+
+if(groomSalary>20000 && groomHeight>5.8){
+    console.log("Groom is Selected");
+}
+else{
+    console.log("Choose another one please");
+}
---
+++ if.js
@@ -0,0 +1,10 @@
+if(3<10){
+    console.log("I have a smaller value");
+}
+var berger = 500;
+if(berger<500){
+    console.log("Give some berger");
+}
+else{
+    console.log("Don't need thar");
+}
