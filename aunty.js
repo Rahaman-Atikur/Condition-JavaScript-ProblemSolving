15b186bfa432d2c97a120ee70099fd8791b7e41b
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-01 16:29:00 +0100

    more about conditions

---
+++ aunty.js
@@ -0,0 +1,18 @@
+/* Conditions
+1. and && 
+2. or ||
+*/
+console.log(5<10);
+console.log(5==10);
+console.log(10>=10);
+
+var applePrice = 300;
+var guavaPrice = 230;
+if (applePrice < guavaPrice)
+    console.log("Yes applePrice are expensive");
+else
+    console.log("Guava price are not expensive");
+
+// same value and type
+console.log(10=='10');
+console.log(10==='10');
